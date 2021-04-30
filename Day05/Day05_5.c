8da3038c4d3f452d591ea63f36afba112d47f413
46748_Sharad_Patil <Sharad.6030@gmail.com>
2021-04-30 19:46:34 +0530

    Reshuffled files

--- Day05/Day05_5.c
+++ Day05/Day05_5.c
@@ -5,20 +5,22 @@ int main()
     char ch;
     double d1;
     float fvar; 
-
     // fact of 3 -> 6 
     // fact of 5 -> 120   
     // fact of 10000   
 
-    short int num1; 
-    num1 = 32767; 
-    printf("num1 = %d\n",num1);
+   // short int num1; 
+   long  long int num1;
+    num1 = 32768; 
+    printf("num1 = %lld\n",num1);
 
      printf("shortint=%d\n",sizeof(num1));
     printf("num=%d\n",sizeof(num));
     printf("char=%d\n",sizeof(ch));
     printf("double=%d\n",sizeof(d1));
     printf("fvar=%d\n",sizeof(fvar));
-
+    printf("By default size of fractional number is = %d",sizeof(10.20));
+    printf("\nsize of fractional number is = %d",sizeof(10.20f));
+    printf("\nsize of fractional number is = %d",sizeof(10.20F));
     return 0;
 }
--- Day05/Day05_6.c
+++ Day05/Day05_6.c
@@ -1,31 +1,30 @@
 #include<stdio.h>
-
 int main()
 {
 
     // if want to print 65 
     // 65 decimal int
     // %d should be used 
-    printf("%d\n",65);
-    printf("%f\n",12.33f);
-    printf("%lf\n",10.33);
-    printf("%c\n",'A');
-    printf("%o\n",010);
-    printf("%d\n",010);  
-    // octal representation of 010 in decimal %d
+   // printf("%d\n",65);
+   // printf("%.2f\n",12.33f);
+    //printf("%.8lf\n",10.3312121212121212);
+    //printf("%c\n",'A');
+    printf("%o\n",0101);
+    printf("%d\n",0101);  
+    // octal representation of 0101 in decimal %d
 
     // I want print decimal representation of 'A'
-    printf("%d\n",'A'); //65 
-    printf("%x\n",0x23); // 23
+  //  printf("%d\n",'A'); //65 
+    printf("%x\n",0x10); // 23
 
-    printf("%d\n",0x23); // hex to decimal 
+    printf("%d\n",0x10); // hex to decimal 
 
-    printf("%u\n",12u);
+   /* printf("%u\n",12u);
     printf("%u\n",12U);
 
     printf("%ld\n",100L); // long
     printf("%ld",100l); // long
-
+*/
 
 
     return 0;
